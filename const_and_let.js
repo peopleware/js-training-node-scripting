3c9cf063b0578c85362a81d76b5e84bb6faae9b2
Jan Dockx <Jan_Dockx@peopleware.be>
2017-08-03 14:06:33 +0200

    Add weird variable names to let and const example

--- const_and_let.js
+++ const_and_let.js
@@ -7,3 +7,11 @@ let letMessage = "Hello, World!";
 console.log(letMessage);
 letMessage = "Goodbye, World!";
 console.log(letMessage);
+
+const é = "Accented";
+console.log(é);
+const 学习 = "Learn";
+console.log(学习);
+// const ∃ = "There exists"; CRASH
+// const 💹 = "Market"; CRASH
+// const 😱 = "Eep!"; CRASH
