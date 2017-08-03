f796b6552aab3f4a906b6439a5b16f4b4bf1b557
Jan Dockx <Jan_Dockx@peopleware.be>
2017-08-03 13:35:53 +0200

    Add var and hoisted example

---
+++ hoisted.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+function test() {
+  var i;
+  var deep;
+
+  console.log("test start i ALREADY EXISTS:", i);
+  console.log("test start deep ALREADY EXISTS:", deep);
+
+  for (i = 0; i < 10; i++) {
+    console.log("loop start i:", i);
+    console.log("loop start deep:", i);
+    if (i >= 100) {
+      if (i < 50) {
+        deep = "lala";
+      }
+    }
+    console.log("loop end i:", i);
+    console.log("loop end deep:", deep);
+  }
+
+  console.log("test end i STILL EXISTS:", i);
+  console.log("test end deep STILL EXISTS:", deep);
+
+  return i; // 10
+}
+
+console.log("test result:", test());
+// console.log("end i", i); CRASH
+// console.log("end deep", deep); CRASH
