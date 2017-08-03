a91066d64ae03cedf8caef4c277e0f8a2af068ee
Jan Dockx <jan_dockx@peopleware.be>
2017-08-03 13:25:45 +0200

    Add const and let example

---
+++ const_and_let.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const constMessage = "Hello, World!";
+console.log(constMessage);
+
+let letMessage = "Hello, World!";
+console.log(letMessage);
+letMessage = "Goodbye, World!";
+console.log(letMessage);
